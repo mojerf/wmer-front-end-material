a24349abf41bd6811f369c6fff46e9bee01b65c3
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-09-23 18:20:49 +0330

    feat(navigation): added a full navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+const MENU = [
+  {
+    url: '#',
+    icon: 'home',
+    name: 'صفحه اصلی',
+  },
+  {
+    url: '#',
+    icon: 'work',
+    name: 'نمونه کار ها',
+  },
+  {
+    url: '#',
+    icon: 'description',
+    name: 'پست ها',
+  },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'wmer';
+
+  menu = MENU;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MatRippleModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
