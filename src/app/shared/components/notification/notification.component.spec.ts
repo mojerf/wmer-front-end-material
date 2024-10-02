c5e5fcadd07e7f79c011a6e5d60a6999bd2b5d35
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-10-02 12:14:18 +0330

    fix(test)

--- src/app/shared/components/notification/notification.component.spec.ts
+++ src/app/shared/components/notification/notification.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotificationComponent } from './notification.component';
+import {
+  MAT_SNACK_BAR_DATA,
+  MatSnackBarModule,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
@@ -8,9 +13,19 @@ describe('NotificationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NotificationComponent]
-    })
-    .compileComponents();
+      declarations: [NotificationComponent],
+      imports: [MatSnackBarModule],
+      providers: [
+        {
+          provide: MatSnackBarRef,
+          useValue: {},
+        },
+        {
+          provide: MAT_SNACK_BAR_DATA,
+          useValue: {}, // Add any data you wish to test if it is passed/used correctly
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
